a4fb55c04f7cddbab7b0a3235b2f6cfd9b974622
Novurran <postwarpoem166@gmail.com>
2020-06-02 15:24:58 -0400

    initial commit

---
+++ Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace WindowTest
+{
+    public class Game
+    {
+        GameWindow window;
+
+        public Game(GameWindow window) {
+            this.window = window;
+            window.Title = "OpenTK Window Test";
+
+            onStart();
+        }
+
+        void onStart()  {
+            window.Load += load;
+            window.RenderFrame += render;
+
+            window.Run(1/60);
+        }
+
+        void render(object o, EventArgs e)  {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            window.SwapBuffers();
+        }
+
+        void load(object o, EventArgs e) {
+            GL.ClearColor(0, 0, 0, 0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenTK;
+
+namespace WindowTest
+{
+    class MainClass {
+        static GameWindow window;
+        static Game game;
+
+        public static void Main(string[] args)
+        {
+            window = new GameWindow(1000, 750);
+            game = new Game(window);
+        }
+    }
+}
